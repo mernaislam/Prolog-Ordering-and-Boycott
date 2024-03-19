7cd0503b7d36ca94df16465414d136162f5cb03c
Merna Islam <101946404+mernaislam@users.noreply.github.com>
2024-03-19 22:00:52 +0200

    q 5, 6, 11 done

--- rules.pl
+++ rules.pl
@@ -1,1 +1,52 @@
 :-consult(data).
+
+% question 5
+getPrice(Item, ItemPrice):-
+    item(Item, X, ItemPrice).
+
+iterate([], 0).
+
+iterate([S|E], TotalPrice):-
+    getPrice(S, ItemPrice),
+    iterate(E, RestPrice),
+    TotalPrice is ItemPrice + RestPrice.
+
+calcPriceOfOrder(Username, OrderID, TotalPrice):-
+    customer(CustID, Username),
+    order(CustID, OrderID, Items),
+    iterate(Items, TotalPrice).
+
+% question 6
+isBoycott(X):-
+    boycott_company(X, Z);
+    alternative(X, Y).
+
+
+% question 11
+getTheDifferenceInPriceBetweenItemAndAlternative(Item, Alternative, DiffPrice):-
+    item(Item, X, ItemPrice),
+    alternative(Item, Alternative),
+    item(Alternative, Y, AltPrice),
+    DiffPrice is ItemPrice - AltPrice.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
