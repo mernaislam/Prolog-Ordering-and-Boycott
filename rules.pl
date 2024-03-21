3aa86e249a16fb2f6ca97b266f2141cd8d44371a
Mohamed Waleed <mohamed.waleed1023@gmail.com>
2024-03-21 16:37:15 +0200

    1, 2, 3 Done
    Fixed singleton variables.

--- rules.pl
+++ rules.pl
@@ -1,8 +1,35 @@
 :-consult(data).
 
+% question 1
+list_orders(Name, L):-
+    customer(ID, Name),
+    getOrders(ID, L, 1),!.
+
+getOrders(ID, L, OrderID):-
+    order(ID, OrderID, Items),
+    NewOrderID is OrderID + 1,
+    getOrders(ID, NewL, NewOrderID),
+    L = [order(ID, OrderID, Items)| NewL].
+getOrders(_, [], _).
+
+% question 2
+countOrdersOfCustomer(Name, Count):-
+    list_orders(Name, Orders),
+    count(Orders, Count).
+
+count([_|T], Count):-
+    count(T, NewCount),
+    Count is NewCount + 1.
+count([], 0).
+
+% question 3
+getItemsInOrderById(Name, OrderID, Items):-
+    customer(ID, Name),
+    order(ID, OrderID, Items), !.
+
 % question 5
 getPrice(Item, ItemPrice):-
-    item(Item, X, ItemPrice).
+    item(Item, _, ItemPrice).
 
 iterate([], 0).
 
@@ -18,15 +45,15 @@ calcPriceOfOrder(Username, OrderID, TotalPrice):-
 
 % question 6
 isBoycott(X):-
-    boycott_company(X, Z);
-    alternative(X, Y).
+    boycott_company(X, _);
+    alternative(X, _).
 
 
 % question 11
 getTheDifferenceInPriceBetweenItemAndAlternative(Item, Alternative, DiffPrice):-
-    item(Item, X, ItemPrice),
+    item(Item, _, ItemPrice),
     alternative(Item, Alternative),
-    item(Alternative, Y, AltPrice),
+    item(Alternative, _, AltPrice),
     DiffPrice is ItemPrice - AltPrice.
 
 
